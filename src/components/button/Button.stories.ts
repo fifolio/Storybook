54e70fce98bfe0620290271cbfe8f14ced8ad503
Firas <97522852+fifolio@users.noreply.github.com>
2026-03-13 16:36:00 +0000

    Storybook: Documentation

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -1,39 +1,39 @@
 import Button from './Button';
 
 export default {
-    title: 'Components/Form/Button',
-    component: Button,
-    argTypes: {
-        onClick: { action: 'clicked' }, // automatically logs clicks
-    },
+  title: 'Components/Form/Button',
+  component: Button,
+  tags: ['autodocs'], // this enables the Docs page. (Storybook generates documentation automatically from stories when the autodocs tag is present.)
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 };
 
-// Each story now uses "args" instead of manually calling Button()
 export const Primary = {
-    args: {
-        variant: 'primary',
-        children: 'Primary Button',
-        onClick: () => console.log('Primary button clicked!'),
-    },
+  args: {
+    variant: 'primary',
+    children: 'Primary Button',
+    onClick: console.log('Primary Button Clicked'),
+  },
 };
 
 export const Secondary = {
-    args: {
-        variant: 'secondary',
-        children: 'Secondary Button',
-    },
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button',
+  },
 };
 
 export const Danger = {
-    args: {
-        variant: 'danger',
-        children: 'Danger Button',
-    },
+  args: {
+    variant: 'danger',
+    children: 'Danger Button',
+  },
 };
 
 export const Success = {
-    args: {
-        variant: 'success',
-        children: 'Success Button',
-    },
+  args: {
+    variant: 'success',
+    children: 'Success Button',
+  },
 };
